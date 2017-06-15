35a95f09b4148290abe30540b7885107834518d0
raph <raph.sallib@hotmail.fr>
2017-06-15 17:33:57 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- src/fr/formation/dao/hibernate/PersonneHibernateDAO.java
+++ src/fr/formation/dao/hibernate/PersonneHibernateDAO.java
@@ -8,6 +8,7 @@ import javax.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import fr.ascadis.model.Utilisateur;
 import fr.formation.dao.IPersonneDAO;
 import fr.formation.model.Personne;
 
@@ -21,19 +22,42 @@ public class PersonneHibernateDAO implements IPersonneDAO {
 	@Override
 	public List<Personne> findAll() {
 		// TODO Auto-generated method stub
+		try {
+			return this.entityManager.createQuery("from Personne", Personne.class).getResultList();
+		}
+		
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
 		return null;
 	}
 
 	@Override
 	public Personne find(String id) {
-		// TODO Auto-generated method stub
+		try {
+			return this.entityManager.find(Personne.class, id);
+		}
+		
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
 	@Override
-	public Personne save(Personne obj) {
-		// TODO Auto-generated method stub
-		return null;
+	public Personne save(Personne personne) {
+		try {
+			return this.entityManager.merge(personne);
+		}
+		
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return personne;
 	}
 
 	@Override
